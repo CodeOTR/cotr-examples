b331d9b261de059a2d226653e66fd4aa24e55c3a
Joseph Muller <jtmuller5@gmail.com>
2023-06-27 09:53:42 -0400

    Sign in simple

--- cotr_google_sign_in/lib/sign_in.dart
+++ cotr_google_sign_in/lib/sign_in.dart
@@ -1,52 +1,87 @@
-import 'package:cotr_google_sign_in/home.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-class SignIn extends StatelessWidget {
+class SignIn extends StatefulWidget {
   const SignIn({Key? key}) : super(key: key);
 
   @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+
+  bool loading = false;
+  GoogleSignInAccount? user;
+
+  @override
   Widget build(BuildContext context) {
+
+    GoogleSignIn googleSignIn = GoogleSignIn(
+      clientId: const String.fromEnvironment('GOOGLE_CLIENT_ID'),
+    );
+
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ElevatedButton(
-              onPressed: () async {
-                GoogleSignIn googleSignIn = GoogleSignIn(
-                  clientId: const String.fromEnvironment('GOOGLE_CLIENT_ID'),
-                );
+      body: Center(
+        child: SizedBox(
+          width: 500,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if(loading) const LinearProgressIndicator(),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () async {
 
-                await googleSignIn.signInSilently();
-                debugPrint('User: ' + googleSignIn.currentUser.toString());
-
-                bool hasAuthorization = await googleSignIn.canAccessScopes(['https://www.googleapis.com/auth/user.birthday.read']);
-
-                debugPrint('hasAuthorization: ' + hasAuthorization.toString());
-
-                if (!hasAuthorization) {
-                  await googleSignIn.requestScopes(['https://www.googleapis.com/auth/user.birthday.read']);
+                  await googleSignIn.signInSilently();
+                  debugPrint('User: ' + googleSignIn.currentUser.toString());
 
                   bool hasAuthorization = await googleSignIn.canAccessScopes(['https://www.googleapis.com/auth/user.birthday.read']);
 
                   debugPrint('hasAuthorization: ' + hasAuthorization.toString());
-                }
-              },
-              child: Text('Check User')),
-          ElevatedButton(
-              onPressed: () async {
-                GoogleSignIn googleSignIn = GoogleSignIn(
-                  clientId: const String.fromEnvironment('GOOGLE_CLIENT_ID'),
-                );
 
-                await googleSignIn.signIn();
+                  if (!hasAuthorization) {
+                    await googleSignIn.requestScopes(['https://www.googleapis.com/auth/user.birthday.read']);
 
-                if (googleSignIn.currentUser != null) {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => const Home()));
-                }
-              },
-              child: Text('Continue with Google'))
-        ],
+                    bool hasAuthorization = await googleSignIn.canAccessScopes(['https://www.googleapis.com/auth/user.birthday.read']);
+
+                    debugPrint('hasAuthorization: ' + hasAuthorization.toString());
+                  }
+                },
+                child: const Text('Check User'),
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () async {
+
+                  setState(() {
+                    loading = true;
+                  });
+                  await googleSignIn.signIn();
+
+                  if (googleSignIn.currentUser != null) {
+                    debugPrint('User: ' + googleSignIn.currentUser.toString());
+                    setState(() {
+                      user = googleSignIn.currentUser;
+                      loading = false;
+                    });
+                  }
+                },
+                child: const Text('Continue with Google'),
+              ),
+              if(user != null)...[
+                const SizedBox(height: 16),
+                const Divider(),
+                ListTile(
+                  leading: GoogleUserCircleAvatar(identity: user!),
+                  title: Text(user?.displayName ?? ''),
+                  subtitle: Text(user?.email ?? ''),
+                )
+              ]
+
+            ],
+          ),
+        ),
       ),
     );
   }
