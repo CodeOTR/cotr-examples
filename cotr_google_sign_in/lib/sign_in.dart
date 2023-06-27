1f872370f425f939997cf626792ced99860ba9c7
Joseph Muller <jtmuller5@gmail.com>
2023-06-27 11:30:06 -0400

    Update sign_in.dart

--- cotr_google_sign_in/lib/sign_in.dart
+++ cotr_google_sign_in/lib/sign_in.dart
@@ -9,13 +9,11 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
-
   bool loading = false;
   GoogleSignInAccount? user;
 
   @override
   Widget build(BuildContext context) {
-
     GoogleSignIn googleSignIn = GoogleSignIn(
       clientId: const String.fromEnvironment('GOOGLE_CLIENT_ID'),
     );
@@ -28,11 +26,10 @@ class _SignInState extends State<SignIn> {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              if(loading) const LinearProgressIndicator(),
-              SizedBox(height: 16),
+              if (loading) const LinearProgressIndicator(),
+              const SizedBox(height: 16),
               ElevatedButton(
                 onPressed: () async {
-
                   await googleSignIn.signInSilently();
                   debugPrint('User: ' + googleSignIn.currentUser.toString());
 
@@ -53,7 +50,6 @@ class _SignInState extends State<SignIn> {
               const SizedBox(height: 16),
               ElevatedButton(
                 onPressed: () async {
-
                   setState(() {
                     loading = true;
                   });
@@ -69,16 +65,79 @@ class _SignInState extends State<SignIn> {
                 },
                 child: const Text('Continue with Google'),
               ),
-              if(user != null)...[
+              if (user != null) ...[
                 const SizedBox(height: 16),
-                const Divider(),
-                ListTile(
-                  leading: GoogleUserCircleAvatar(identity: user!),
-                  title: Text(user?.displayName ?? ''),
-                  subtitle: Text(user?.email ?? ''),
+                DecoratedBox(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(color: Colors.grey.shade400),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 16.0, bottom: 16),
+                    child: Column(
+                      children: [
+                        ListTile(
+                          leading: GoogleUserCircleAvatar(identity: user!),
+                          title: Text(user?.displayName ?? ''),
+                          subtitle: Text(user?.email ?? ''),
+                        ),
+                        FutureBuilder(
+                            future: user!.authHeaders,
+                            builder: (context, headers) {
+                              return Column(
+                                children: [
+                                  const ListTile(
+                                    leading: Icon(Icons.key),
+                                    title: Text('Auth Headers'),
+                                  ),
+                                  Row(
+                                    children: [
+                                      const SizedBox(
+                                        width: 48,
+                                      ),
+                                      Expanded(child: Text(headers.data.toString())),
+                                    ],
+                                  ),
+                                ],
+                              );
+                            }),
+                        FutureBuilder<GoogleSignInAuthentication>(
+                            future: user!.authentication,
+                            builder: (context, auth) {
+                              return Column(
+                                children: [
+                                  const ListTile(
+                                    leading: Icon(Icons.person),
+                                    title: Text('ID Token'),
+                                  ),
+                                  Row(
+                                    children: [
+                                      const SizedBox(
+                                        width: 48,
+                                      ),
+                                      Expanded(child: Text(auth.data?.idToken ?? '')),
+                                    ],
+                                  ),
+                                  const ListTile(
+                                    leading: Icon(Icons.lock),
+                                    title: Text('Access Token'),
+                                  ),
+                                  Row(
+                                    children: [
+                                      const SizedBox(
+                                        width: 48,
+                                      ),
+                                      Expanded(child: Text(auth.data?.accessToken ?? '')),
+                                    ],
+                                  ),
+                                ],
+                              );
+                            }),
+                      ],
+                    ),
+                  ),
                 )
               ]
-
             ],
           ),
         ),
